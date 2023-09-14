649e829e9b6daf3aba177b84c2af3e483c016ecc
Giole13 <gudwns4f12@gmail.com>
2023-09-14 20:57:47 +0900

    [HyungJun] pool manager

--- MultiShootingGame/Assets/Scripts/Manager/PoolManager.cs
+++ MultiShootingGame/Assets/Scripts/Manager/PoolManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 // 여러 오브젝트 풀링들을 책임지는 클래스
 public class PoolManager : MonoBehaviour
 {
-    // public
+    // 풀링 객체
+    // 딕셔너리에서 바로 접근 후 뽑아 사용하는 형태
+    [SerializeField] private Dictionary<string, Stack<GameObject>> bulletPool;
+
+    // 풀링할 프리팹 리스트
+    [SerializeField] private List<GameObject> bulletList;
+
     private void Awake()
     {
-        int i;
+
     }
 
 
